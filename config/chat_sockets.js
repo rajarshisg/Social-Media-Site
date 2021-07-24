5231a2556c048297352c83ce682f6c66544e98b3
rajarshisg <rajarshisengupta50@gmail.com>
2021-07-25 03:31:27 +0530

    Changed chat_socket.js

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -16,34 +16,3 @@ module.exports.chatSockets = function(socketServer){
     });
 
 }
-
-
-/*module.exports.chatSockets = function(socketServer)
-{
-    let io = require('socket.io')(socketServer);
-
-    io.sockets.on('connection', function(socket)
-    {
-        // console.log('New Connection Received', socket.id);
-
-        socket.on('disconnect', function()
-        {
-            // console.log('Socket Disconnected');
-        });
-
-        
-        socket.on('join_room', function(data)
-        {
-            // console.log('Joining Request Received', data);
-
-            socket.join(data.chatroom);
-
-            io.in(data.chatroom).emit('user_joined', data);
-        });
-
-        socket.on('send_message', function(data)
-        {
-            io.in(data.chatroom).emit('receive_message', data);
-        });
-    });
-}*/
